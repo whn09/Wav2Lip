752d633b124222e5a7ef0cfbd2f4b1fb8b5c2a8e
EC2 Default User <ec2-user@ip-172-16-101-138.ec2.internal>
2023-11-20 05:08:20 +0000

    remove demo.sh

--- demo.sh
+++
@@ -1,6 +0,0 @@
-python inference.py \
-    --checkpoint_path "checkpoints/wav2lip_gan.pth" \
-    --face ../VID20230620105517.mp4 \
-    --audio ../test.wav \
-    --outfile results/result_wav2lip-seamlessClone.mp4 \
-    --resize_factor 1
